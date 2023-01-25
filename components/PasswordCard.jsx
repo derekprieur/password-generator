b5d20b36ee582cbed9957ec24b71176ca60f85ca
derekprieur <DPrieur123@gmail.com>
2023-01-24 19:50:28 -0500

    final 2.0

--- components/PasswordCard.jsx
+++ components/PasswordCard.jsx
@@ -5,6 +5,7 @@ const PasswordCard = ({ password }) => {
     const [isCopied, setIsCopied] = useState(false)
 
     const copyToClipboard = () => {
+        if (password === '') return
         navigator.clipboard.writeText(password)
         setIsCopied(true)
         setTimeout(() => {
@@ -15,7 +16,7 @@ const PasswordCard = ({ password }) => {
     return (
         <div className='w-full bg-[#24232c] mt-4'>
             <div className='flex justify-between items-center px-4 py-4'>
-                <p id='password' className='text-[#E6E5EA] text-2xl'>{password === '' ? 'PTx1f5DaFX' : password}</p>
+                <p id='password' className={`${password === '' ? 'text-[#817d92]' : 'text-[#E6E5EA]'} text-2xl`}>{password === '' ? 'PTx1f5DaFX' : password}</p>
                 <div className='flex gap-4'>
                     {isCopied && <p className='text-[#A4FFAF]'>COPIED</p>}
                     <FaRegCopy className='text-[#a4ffaf] text-xl hover:text-white cursor-pointer' onClick={copyToClipboard} />
--- components/Strength.jsx
+++ components/Strength.jsx
@@ -1,18 +1,50 @@
 import React from 'react'
 
+const colors = {
+    'too weak!': 'bg-[#F64A4A]',
+    'weak': 'bg-[#FB7C58]',
+    'medium': 'bg-[#F8CD65]',
+    'strong': 'bg-[#A4FFAF]'
+}
+
 const Strength = ({ strength }) => {
+    let color = colors[strength.toLowerCase()]
+    console.log(strength.toLowerCase())
     return (
         <div className='text-[#E6E5EA] mt-8'>
             <div className='px-4 bg-[#18171F] py-4 flex items-center justify-between'>
-                <p className='text-[#817D92] text-base'>STRENGTH</p>
+                <p className='text-[#817D92] text-base uppercase'>STRENGTH</p>
                 <div className='flex gap-4 items-center'>
-                    <p className='text-lg'>MEDIUM</p>
-                    <div className='flex gap-2'>
-                        <div className='w-[10px] h-7 bg-[#F8CD65]' />
-                        {strength.toLowerCase() === 'weak' || <div className='w-[10px] h-7 bg-[#F8CD65]' />}
-                        <div className='w-[10px] h-7 bg-[#F8CD65]' />
-                        <div className='w-[10px] h-7 border-2 border-[#E6E5EA]' />
-                    </div>
+                    <p className='text-lg uppercase'>{strength}</p>
+                    {strength === 'Too Weak!' ?
+                        <div className='flex gap-2'>
+                            <div className={`w-[10px] h-7 ${color}`} />
+                            <div className={`w-[10px] h-7 border-2 border-[#E6E5EA]`} />
+                            <div className={`w-[10px] h-7 border-2 border-[#E6E5EA]`} />
+                            <div className='w-[10px] h-7 border-2 border-[#E6E5EA]' />
+                        </div>
+                        : strength === 'Weak' ?
+                            <div className='flex gap-2'>
+                                <div className={`w-[10px] h-7 ${color}`} />
+                                <div className={`w-[10px] h-7 ${color}`} />
+                                <div className={`w-[10px] h-7 border-2 border-[#E6E5EA]`} />
+                                <div className='w-[10px] h-7 border-2 border-[#E6E5EA]' />
+                            </div>
+                            : strength === 'Medium' ?
+                                <div className='flex gap-2'>
+                                    <div className={`w-[10px] h-7 ${color}`} />
+                                    <div className={`w-[10px] h-7 ${color}`} />
+                                    <div className={`w-[10px] h-7 ${color}`} />
+                                    <div className='w-[10px] h-7 border-2 border-[#E6E5EA]' />
+                                </div>
+                                :
+                                <div className='flex gap-2'>
+                                    <div className={`w-[10px] h-7 ${color}`} />
+                                    <div className={`w-[10px] h-7 ${color}`} />
+                                    <div className={`w-[10px] h-7 ${color}`} />
+                                    <div className={`w-[10px] h-7 ${color}`} />
+                                </div>
+                    }
                 </div>
             </div>
         </div>
